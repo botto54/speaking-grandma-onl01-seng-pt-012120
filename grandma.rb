b3972ec74c4b7b54580a82de9b2481e7eb5527fc
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-01 22:00:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase
-    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else phrase
     return "NO, NOT SINCE 1938!"
   end
 end
