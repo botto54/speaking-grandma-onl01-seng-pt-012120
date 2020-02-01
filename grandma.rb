188ce163ce2ec3e1da7dd59ca79344f66ad19988
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-01 22:01:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase
-    return "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!" "HUH?! SPEAK UP, SONNY!"
   end
 end
